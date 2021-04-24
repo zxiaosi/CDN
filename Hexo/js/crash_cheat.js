47f262ed82346931efe6876fcbc7e23c09ca438d
zxiaosi <2946399658@qq.com>
2021-04-24 10:20:18 +0800

    update blog

--- Hexo/js/crash_cheat.js
+++ Hexo/js/crash_cheat.js
@@ -1,9 +1,8 @@
-<!--崩溃欺骗-->
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/zxiaosi/cdn/Hexo/icon/break.webp");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
